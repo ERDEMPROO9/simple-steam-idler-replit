ba30ffc2fbb1326208c7b849ea5aae7b663da3c0
ERDEMPROO9 <100533177+ERDEMPROO9@users.noreply.github.com>
2024-04-22 20:13:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730];  // Enter here AppIDs of the needed games
+var games = [1174180, 271590, 394360, 1118200, 1172620, 924970, 534380];  // Enter here AppIDs of the needed games
 var status = 7;  // 1 - online, 7 - invisible
 
 
